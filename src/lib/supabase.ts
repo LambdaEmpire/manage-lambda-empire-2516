1325b8e3c59a9eb68a3c91a63f374aff6242bd84
Kliv <noreply@kliv.dev>
2025-07-27 19:53:59 +0200

    Update code

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://tvsfdoesslskumujvafd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2c2Zkb2Vzc2xza3VtdWp2YWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3Nzg0MzcsImV4cCI6MjAzNzM1NDQzN30.Hs_lR7_vJlbKGBKGVJBXhJJmJYrGvQHFqGvQHFqGvQHF';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2c2Zkb2Vzc2xza3VtdWp2YWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzMTU2MzcsImV4cCI6MjA2ODg5MTYzN30.n8F8TeqsMpMynOwwdanr5eg2bv7tpDxGGvAYwl7rvOA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
